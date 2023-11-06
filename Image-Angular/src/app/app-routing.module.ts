4919bcdb5f7f76b7f2c03f125db6fc695e6d75de
juniorbueno0 <jjunior.buen00@gmail.com>
2023-11-06 11:21:31 -0700

    profile

--- Image-Angular/src/app/app-routing.module.ts
+++ Image-Angular/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { ContentComponent } from './user/content/content.component';
 import { LoginComponent } from './user/login/login.component';
 import { CreatePostComponent } from './post/modules/create-post/create-post.component';
 import { ExplorePostComponent } from './post/modules/explore-post/explore-post.component';
+import { PostFavoritesComponent } from './post/modules/post-favorites/post-favorites.component';
 
 const routes: Routes = [
   {path:'register',component:RegisterComponent},
@@ -12,6 +13,7 @@ const routes: Routes = [
   {path:'main',component:ContentComponent},
   {path:'create',component:CreatePostComponent},
   {path:'explore',component:ExplorePostComponent},
+  {path:'favorites',component:PostFavoritesComponent},
   {path:'', redirectTo:'register',pathMatch:'full'},
   {path:'**', redirectTo:'register'}
 ];
--- Image-Angular/src/app/post/post.module.ts
+++ Image-Angular/src/app/post/post.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreatePostComponent } from './modules/create-post/create-post.component';
 import { ExplorePostComponent } from './modules/explore-post/explore-post.component';
+import { PostFavoritesComponent } from './modules/post-favorites/post-favorites.component';
 
 @NgModule({
   declarations: [
     CreatePostComponent,
-    ExplorePostComponent
+    ExplorePostComponent,
+    PostFavoritesComponent
   ],
   imports: [
     CommonModule
